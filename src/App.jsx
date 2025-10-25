8c4157e0274352508cc590dea91cbd57927cdf19
don1502 <dongomez1502@gmail.com>
2025-10-25 16:47:25 +0530

    Error handling while fetching data from DB

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <ol>
+            <Link to = "/" style={{ textDecoration : "none", padding:"10px" }}>Home</Link>
+            <Link to = "product" style={{ textDecoration : "none", padding:"10px" }}>Products</Link>
+      </ol>
         <Routes>
           <Route path='/' element ={<Home/>}/>
           <Route path='/product' element = { <Product/>}/>
         </Routes>
-        <ol>
-          <li>
-            <Link to = "product" style={{ textDecoration : "none" }}>Products</Link>
-          </li>
-        </ol>
       </BrowserRouter>
     </>
   )
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,36 +4,57 @@ import Card from 'react-bootstrap/Card';
 
 const Product = () => {
     let [product, setProduct] = useState([]);
+    let [error, setError] = useState("");
+    let [isLoading, setIsLoading] = useState( true )
 
     useEffect( ()=> {
-        fetch("http://localhost:3000/products", {method : "GET"})
-        .then((response ) => {return response.json()})
-        .then((data) => { setProduct(data);}) 
-},[])
+        fetch("http://localhost:3000/products", { method : "GET"})
+        .then(( response ) => {if(response.ok){return response.json()}
+                                else{ return new Error( "Search proper data" ) }})
+        .then(( data ) => { setProduct( data );}) 
+        .catch( ( error )=>{ setError( error.message ); })
+        .finally(()=>{ setIsLoading( false ); })
+            },[])
     
+    if(isLoading){
+        return ( 
+            <>
+                <h1 style={{ display:'flex', justifyContent:'center', color:"blueviolet" }}> loading...</h1>
+            </>
+         )
+    }
+
     return (
         <>
             <h1>Product</h1>
-            <section className="productSection">
-                {
-                    product.map(product => (
-                        <Card style={{ width: '18rem',height:"25rem", border:"1px solid black", borderRadius:"5px"}} className="productCard">
-                            <center>
-                            <Card.Img variant="top" src={product.image} className="productImage" />
-                            </center>
-                            <Card.Body>
-                                <Card.Title>{product.title}</Card.Title>
-                                <Card.Text style={{overflow:"scroll", height:"100px"}}>{product.description}</Card.Text>
-                            </Card.Body>
-                            <Card.Footer style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"5px"}}>
-                                <Card.Text >{product.price}</Card.Text>
-                                    <Button variant="primary">Add to Cart</Button>
-                            </Card.Footer>
-                        </Card>
-                    ))
-                }
-            </section>
-
+            {
+                product.length !==0 && (
+                    <section className="productSection">
+                        {
+                            product.map(product => (
+                                <Card key = {product.id} style={{ width: '18rem',height:"25rem", border:"1px solid black", borderRadius:"5px"}} className="productCard">
+                                    <center>
+                                        <Card.Img variant="top" src={product.image} className="productImage" />
+                                    </center>
+                                <Card.Body>
+                                    <Card.Title>{product.title}</Card.Title>
+                                    <Card.Text style={{overflow:"scroll", height:"100px"}}>{product.description}</Card.Text>
+                                </Card.Body>
+                                <Card.Footer style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"5px"}}>
+                                    <Card.Text >{product.price}</Card.Text>
+                                        <Button variant="primary">Add to Cart</Button>
+                                </Card.Footer>
+                            </Card>
+                            ))
+                        }
+                    </section>
+                )
+            }
+            {
+                error && 
+                <p style={{ fontSize:"7rem",display:"flex", justifyContent:"center",padding:"10px", color:"blueviolet", backgroundColor: "ghostwhite" }}> 
+                { error } data 🥲</p>
+            }
         </>
     )
 }
